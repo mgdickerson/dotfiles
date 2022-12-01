35669d4adacd40f3e653bd73f08f08908c7e1072
Mitchel Dickerson <mgdicker@uci.edu>
2022-12-01 09:56:47 -0800

    Added tmux setup script.

--- tmux/setup-tmux.sh
+++ tmux/setup-tmux.sh
@@ -3,23 +3,52 @@
 # This script will install (if it is not already) and setup TMUX,
 # TPM, and a default config.
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-  Linux*)   machine=linux;;
-  Darwin*)  machine=macOS;;
-  *)        machine=unknown;;
-esac
+install_deps () {
+  for dep in "$@"; do
+    if command -v $1 &> /dev/null
+    then
+      echo Dependency $1 is already installed.
+      return
+    fi
+    
+    echo Installing $1...
+    case "$(uname -s)" in
+      Linux*)
+        sudo apt-get install $1 -y
+        ;;
+      Darwin*)
+        brew install $1
+        ;;
+      *)
+        echo Currently unsupported system: "$(uname -s)"
+        echo Exiting...
+        exit 1
+        ;;
+    esac
+    echo Install Complete.
+  done
+}
 
-echo ${machine}
+# List of dependencies:
+deps=(
+  tmux
+)
 
-if [[ $OSTYPE == 'darwin'* ]]; then
-  echo 'macOS'
-fi
+# Install Dependencies
+echo Checking Dependencies: ${deps[@]}
+install_deps ${deps[@]}
 
+# Grab Conf
+echo Grabbing Oh My Tmux configuration...
+git clone https://github.com/gpakosz/.tmux.git ${HOME}/Programs/.tmux
+ln -s -f ${HOME}/Programs/.tmux/.tmux.conf ${HOME}
 
-# Setup script for tmux (assuming TMUX is installed):
-if ! command -v tmux &> /dev/null
-then
-  echo "tmux not installed, install tmux."
-  exit
-fi
+# Get TPM
+echo Getting TPM...
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Copy Local Conf
+echo Copy local configuration...
+cp .tmux.conf.local ${HOME}/.
+
+echo Done!
